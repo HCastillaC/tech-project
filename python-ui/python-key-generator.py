1a3e609db38108472d2d92afdf6bf9fbe2508d39
HCastillaC <hugocc0113@gmail.com>
2025-05-12 18:57:09 +0200

    Working-expand

--- python-ui/python-key-generator.py
+++ python-ui/python-key-generator.py
@@ -1,41 +1,63 @@
 import tkinter as tk
 
+#Important things
+padding = 5
+
 window = tk.Tk()
 
-frame_control = tk.Frame()
-button_foward = tk.Button(
+frame_control = tk.Frame(master=window, width=200, height=100, bg="red")
+button_fwd = tk.Button(
     master=frame_control,
-    text='F'
+    text='F',
+    bg='white'
 )
-button_backward = tk.Button(
+button_bwd = tk.Button(
     master=frame_control,
-    text='B'
+    text='B',
+    bg='white'
 )
 button_left = tk.Button(
     master=frame_control,
-    text='L'
+    text='L',
+    bg='white'
 )
 button_right = tk.Button(
     master=frame_control,
-    text='R'
+    text='R',
+    bg='white'
 )
+frame_control.columnconfigure([0, 1, 2], weight=1, minsize=20)
+frame_control.rowconfigure([0, 1], weight=1, minsize=20)
 
-button_foward.grid(row=0,column=1)
-button_backward.grid(row=1,column=1)
-button_left.grid(row=1,column=0)
-button_right.grid(row=1,column=2)
-frame_control.pack()
 
-frame_input_output = tk.Frame()
+button_fwd.grid(column=1, row=0, sticky="nsew", padx=padding, pady=padding)
+button_bwd.grid(column=1, row=1, sticky="nsew", padx=padding, pady=padding)
+button_left.grid(column=0, row=1, sticky="nsew", padx=padding, pady=padding)
+button_right.grid(column=2, row=1, sticky="nsew", padx=padding, pady=padding)
+
+frame_control.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+
+frame_io = tk.Frame(master=window, width=100, height=100, bg="yellow")
+frame_input = tk.Frame(master=frame_io, width=100, height=50, bg="green")
+label_input = tk.Label(
+    master=frame_input,
+    text='Enter speed/turn radius here:'
+)
 entry_input = tk.Entry(
-    master=frame_input_output
+    master=frame_input,
 )
-entry_output = tk.Label(
-    master=frame_input_output,
-    text="Fuck y'all"
+label_output = tk.Label(
+    master=frame_io,
+    text='Fucking hell'
 )
-entry_input.grid(row=0,column=0)
-entry_output.grid(row=1,column=0)
-frame_input_output.pack()
+frame_io.columnconfigure(0, weight=1, minsize=20)
+frame_io.rowconfigure([0, 1], weight=1, minsize=20)
+frame_input.columnconfigure(1, weight=1, minsize=20)
+frame_input.rowconfigure(0, weight=1, minsize=20)
+label_input.grid(column=0, row=0, sticky="nsew", padx=padding, pady=padding)
+entry_input.grid(column=1, row=0, sticky="nsew", padx=padding, pady=padding)
+frame_input.grid(column=0, row=0, sticky="nsew", padx=padding, pady=padding)
+label_output.grid(column=0, row=1, sticky="nsew", padx=padding, pady=padding)
+frame_io.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
 window.mainloop()
